e7a2edc6f707efc35319a7513360c8545c903df9
WaI2k2 <buithanh2342002@gmail.com>
2024-05-31 16:08:36 +0700

    Add SingleNumberIII_Solution2

---
+++ Java/WebsiteCode/LeechCode/src/BetterCodeAnswer/Medium/Array/SingleNumberIII.java
@@ -0,0 +1,90 @@
+package BetterCodeAnswer.Medium.Array;
+
+import java.util.Arrays;
+
+/**
+ * <a class="no-underline hover:text-blue-s dark:hover:text-dark-blue-s truncate cursor-text whitespace-normal hover:!text-[inherit]" href="/problems/single-number-iii/">260. Single Number III</a>
+ * 
+ * <div class="elfjS" data-track-load="description_content"><p>Given an integer array <code>nums</code>, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in <strong>any order</strong>.</p>
+ * 
+ * <p>You must write an&nbsp;algorithm that runs in linear runtime complexity and uses&nbsp;only constant extra space.</p>
+ * 
+ * <p>&nbsp;</p>
+ * <p><strong class="example">Example 1:</strong></p>
+ * 
+ * <pre><strong>Input:</strong> nums = [1,2,1,3,2,5]
+ * <strong>Output:</strong> [3,5]
+ * <strong>Explanation: </strong> [5, 3] is also a valid answer.
+ * </pre>
+ * 
+ * <p><strong class="example">Example 2:</strong></p>
+ * 
+ * <pre><strong>Input:</strong> nums = [-1,0]
+ * <strong>Output:</strong> [-1,0]
+ * </pre>
+ * 
+ * <p><strong class="example">Example 3:</strong></p>
+ * 
+ * <pre><strong>Input:</strong> nums = [0,1]
+ * <strong>Output:</strong> [1,0]
+ * </pre>
+ * 
+ * <p>&nbsp;</p>
+ * <p><strong>Constraints:</strong></p>
+ * 
+ * <ul>
+ * 	<li><code>2 &lt;= nums.length &lt;= 3 * 10<sup>4</sup></code></li>
+ * 	<li><code>-2<sup>31</sup> &lt;= nums[i] &lt;= 2<sup>31</sup> - 1</code></li>
+ * 	<li>Each integer in <code>nums</code> will appear twice, only two integers will appear once.</li>
+ * </ul>
+ * </div>
+ */
+public class SingleNumberIII {
+    public static void main(String[] args) {
+        int[][] tests = {
+            {0,1,1,2},
+            {0,1,2,2},
+            {1,2,1,3,2,5}
+        };
+
+        for (int[] nums : tests) {
+            System.out.println(Arrays.toString(new SingleNumberIII_Solution().singleNumber(nums)));
+        }
+    }
+}
+
+// 1 ms 46 MB
+class SingleNumberIII_Solution {
+    public int[] singleNumber(int[] nums) {
+        int ans =0 ;
+        for(int i= 0; i < nums.length;i++)
+        {
+            ans = ans ^ nums[i];
+        }
+        ans &= -ans;
+        int[] result = new int[2];
+        for(int n: nums)
+        {
+            if((ans & n) == 0)result[0] ^= n;
+            else result[1] ^= n;
+        }
+        return result;
+    }
+}
+
+// 2 ms 45.8 MB
+class SingleNumberIII_Solution2 {
+    public int[] singleNumber(int[] nums) {
+        final int xors = Arrays.stream(nums).reduce((a, b) -> a ^ b).getAsInt();
+        final int lowbit = xors & -xors;
+        int[] ans = new int[2];
+
+        for (final int num : nums)
+        if ((num & lowbit) > 0)
+            ans[0] ^= num;
+        else
+            ans[1] ^= num;
+
+        return ans;
+    }
+}
