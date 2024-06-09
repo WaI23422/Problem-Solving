4c7f087f08aee9a6dd69ad64ea4fb7d0354ad54a
WaI2k2 <buithanh2342002@gmail.com>
2024-06-09 10:52:57 +0700

    Update SubarraySumsDivisibleByK_Solution

--- Java/WebsiteCode/LeechCode/src/BetterCodeAnswer/Medium/Array/SubarraySumsDivisibleByK.java
+++ Java/WebsiteCode/LeechCode/src/BetterCodeAnswer/Medium/Array/SubarraySumsDivisibleByK.java
@@ -58,22 +58,36 @@ public class SubarraySumsDivisibleByK {
 // 2 ms 45.7 MB
 class SubarraySumsDivisibleByK_Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        int count=0,sum=0,rem;
-        int md[]=new int[k];
-        for(int i=0;i<nums.length;i++)
-        {
-            sum+=nums[i];
-            rem=sum%k;
-            if(rem<0)
-            rem+=k;
-            md[rem]++;
+        // Use counting array to record the frequency of all the prefix sum remainders.
+        int[] counting = new int[k];
+        for (int i = 0, remainder = 0; i < nums.length; i++) {
+            // Note that the integer in 'nums' can be negative.
+            // Thus, we need to adjust the negative remainder to positive remainder.
+            // Below accounts for both negative and positive remainders.
+            // We can also check if the remainder is negative, then add a 'k' to make the remainder positive.
+            // For Example, nums = [-2,3,2], k = 5,
+            // remainder for the prefix sum of [-2,1,3] are -2, 1 and 3 respectively.
+            // We know that [3,2] sum to 5, which is divisible by 5.
+            // After converting -2 to 3, by adding 5, it has the same remainder with prefix sum 3.
+            remainder = ((remainder + nums[i]) % k + k) % k;
+            counting[remainder]++;
         }
-        for(int i=0;i<k;i++)
-        {
-            if(md[i]>1)
-                count+=(md[i]*(md[i]-1))/2;
-        }
-        count+=md[0];
-        return count;
+
+        // The result contains all the prefix sum with remainder 0,
+        // as all the prefix sum with remainder of 0 is itself divisible by 'k'.
+        // However, do note that the prefix sum with remainder 0 also able to form subarray sums that is divisible by 'k'
+        // with one another, which will be calculated next.
+        // For Example: nums = [5,5,5,5], k = 5,
+        // The prefix sum of [5,10,15,20] are themselves divisible by 5, while also forming subarray sums divisible by 5
+        // with 10 [5,5] - 5 [5] == 5, 15 [5,5,5] - 5 [5] == 10, etc.
+        int result = counting[0];
+
+        // The prefix sums with the same remainder can form subarray sums that is divisible by 'k' with each other.
+        // For each remainder, the number of subarray that is divisible by 'k' is the number of combinations from the frequency.
+        // Equation for the number of combinations of n items is n * "(n - 1) / 2".
+        for (int frequency : counting)
+            result += frequency * (frequency - 1) / 2;
+
+        return result;
     }
 }
