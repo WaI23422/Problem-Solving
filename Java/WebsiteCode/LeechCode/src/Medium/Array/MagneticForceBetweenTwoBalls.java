9ecae09db573839da4a31d47ae76ace0b1de5dad
WaI2k2 <buithanh2342002@gmail.com>
2024-06-20 07:30:48 +0700

    Add MagneticForceBetweenTwoBalls

---
+++ Java/WebsiteCode/LeechCode/src/Medium/Array/MagneticForceBetweenTwoBalls.java
@@ -0,0 +1,50 @@
+package Medium.Array;
+
+import java.util.Arrays;
+
+/**
+ * 
+ */
+public class MagneticForceBetweenTwoBalls {
+    public static void main(String[] args) {
+        int[][][] tests = {
+            {
+                {1,2,3,4,7},
+                {3}
+            },
+        };
+
+        for (int[][] test : tests) {
+            int position[] = test[0],
+                m = test[1][0];
+            
+            System.out.println(new MagneticForceBetweenTwoBalls_Solution().maxDistance(position, m));
+        }
+    }
+}
+
+// 41 ms 56 MB
+class MagneticForceBetweenTwoBalls_Solution {
+    public int maxDistance(int[] position, int m) {
+        Arrays.sort(position);
+        int l = 1, r = position[position.length - 1] - position[0];
+        int ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int lastPosition = position[0], balls = 1;
+            for (int i = 1; i < position.length; i++) {
+                if (position[i] - lastPosition >= mid) {
+                    lastPosition = position[i];
+                    balls++;
+                }
+            }
+            if (balls >= m) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
