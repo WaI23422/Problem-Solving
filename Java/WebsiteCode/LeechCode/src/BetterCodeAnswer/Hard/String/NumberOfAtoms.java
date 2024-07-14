360cb0e9a5fedd0d5cdf05f4f71d4cfcdb9be669
WaI2k2 <buithanh2342002@gmail.com>
2024-07-14 12:13:39 +0700

    Add NumberOfAtoms_Solution

--- Java/WebsiteCode/LeechCode/src/BetterCodeAnswer/Hard/String/NumberOfAtoms.java
+++ Java/WebsiteCode/LeechCode/src/BetterCodeAnswer/Hard/String/NumberOfAtoms.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.SortedMap;
 import java.util.TreeMap;
 
 /**
@@ -84,7 +83,7 @@ public class NumberOfAtoms {
     }
 }
 
-// 11 ms 42.3 MB
+// 2 ms 41.7 MB
 class NumberOfAtoms_Solution {
     public String countOfAtoms(String formula) {
         Map<String, Integer> map = new HashMap<>();
@@ -126,5 +125,136 @@ class NumberOfAtoms_Solution {
             if(f > 1) sb.append(f);
         }
         return sb.toString();
+    }
+}
+
+// 3ms 41.6MB
+class NumberOfAtoms_Solution2 {
+    public static int where;
+    
+    public static String countOfAtoms(String str){
+        where = 0;
+        StringBuilder ans = new StringBuilder();
+        TreeMap<String, Integer> map = f(str.toCharArray(), 0);
+        for (String key : map.keySet()){
+            ans.append(key);
+            if (map.get(key) > 1){
+                ans.append(map.get(key));
+            }
+        }
+        return ans.toString();
+    }
+
+    public static void fill(TreeMap<String, Integer> ans, StringBuilder name, TreeMap<String, Integer> pre, int cnt){
+        if (name.length() != 0 || pre != null){
+            cnt = cnt == 0 ? 1 : cnt;
+            if (name.length() > 0){
+                String key = name.toString();
+                ans.put(key, ans.getOrDefault(key, 0) + cnt);  // 修正此处，不再循环 cnt 次
+            } else {
+                for (String key: pre.keySet()){
+                    ans.put(key, ans.getOrDefault(key, 0) + cnt * pre.get(key));
+                }
+            }
+        }
+    }
+
+    public static TreeMap<String, Integer> f(char[] s, int i){
+        int cur = 0;
+        TreeMap<String, Integer> ans = new TreeMap<>();
+        StringBuilder name = new StringBuilder();
+        TreeMap<String, Integer> pre = null;
+
+        while (i < s.length && s[i] != ')'){
+            if (s[i] == '(' || (s[i] >= 'A' && s[i] <= 'Z')){
+                fill(ans, name, pre, cur);
+                cur = 0;
+                pre = null;
+                name.setLength(0);
+
+                if (s[i] == '('){
+                    pre = f(s, i + 1);
+                    i = where + 1;
+                } else {
+                    name.append(s[i++]);
+                }
+
+            } else if (s[i] - '0' >= 0 && s[i] - '0' <= 9){
+                cur = cur * 10 + s[i++] - '0';
+            } else {
+                name.append(s[i++]);
+            }
+        }
+        where = i;
+        fill(ans, name, pre, cur);
+        return ans;
+    }
+}
+
+// 4ms 42MB
+class NumberOfAtoms_Solution3 {
+    public String countOfAtoms(String formula) {
+        List<Pair> list = new ArrayList<>();
+        int i = 0;
+        while (i < formula.length()) {
+            char ch = formula.charAt(i);
+            if (Character.isUpperCase(ch)) {
+                StringBuilder elementToAdd = new StringBuilder();
+                int count = 0;
+                elementToAdd.append(ch + ""); i++;
+                while (i < formula.length() && Character.isLowerCase(formula.charAt(i))) {
+                    elementToAdd.append(formula.charAt(i));
+                    i++;
+                }
+                while (i < formula.length() && Character.isDigit(formula.charAt(i))) {
+                    count = count * 10 + formula.charAt(i) - '0';
+                    i++;
+                }
+                count = count == 0 ? 1 : count;
+                list.add(new Pair(elementToAdd.toString(), count));
+            } else if (ch == '(') {
+                list.add(new Pair("(", 0));
+                i++;
+            } else if (ch == ')') {
+                i++; int count = 0;
+                while (i < formula.length() && Character.isDigit(formula.charAt(i))) {
+                    count = count * 10 + formula.charAt(i) - '0';
+                    i++;
+                }
+                count = count == 0 ? 1 : count;
+                int j = list.size()-1;
+                while (j >= 0 && list.get(j).element != "(") {
+                    list.get(j).count *= count;
+                    j--;
+                }
+                list.remove(j);
+            }
+        }
+        Map<String, Integer> countMap = new TreeMap<>();
+        for (Pair pair : list) {
+            if (pair.element == "(") continue;
+            countMap.put(pair.element, countMap.getOrDefault(pair.element, 0) + pair.count);
+        }
+        StringBuilder res = new StringBuilder();
+        for (String element : countMap.keySet()) {
+            String count = countMap.get(element) == 1 ?  "" : String.valueOf(countMap.get(element));
+            res.append(element);
+            res.append(count);
+        }
+        return res.toString();
+    }
+}
+
+class Pair {
+    String element;
+    int count;
+
+    public Pair(String element, int count) {
+        this.element = element;
+        this.count = count;
+    }
+
+    public String toString() {
+        return element + ":" + count;
     }
 }
