a7d564d450bc1a1ccf21363a154fc5d8831e04b3
WaI2k2 <buithanh2342002@gmail.com>
2024-07-22 09:53:03 +0700

    Fix SortThePeople_Solution

--- Java/WebsiteCode/LeechCode/src/BetterCodeAnswer/Easy/Arrays/SortThePeople.java
+++ Java/WebsiteCode/LeechCode/src/BetterCodeAnswer/Easy/Arrays/SortThePeople.java
@@ -78,72 +78,73 @@ class SortThePeople_Solution {
                 }
             }
         }
-        private <T> void quickSortReverseOrder(int[] main, T[] side, int left, int right) {
-            if(left >= right) return;
-            if(right - left <= 11) {
-                // Do insertion sort
-                insertionSortReverseOrder(main, side, left, right);
-                return;
-            }
-            // Choose pivot of median between left, left+1, left+2
-            int pivot;
-            {
-                int aV = main[left], bV = main[left+1], cV = main[left+2];
-                if(aV <= bV && aV <= cV) {
-                    // a is smallest
-                    pivot = bV <= cV ? (left+1) : (left+2);
-                }else if(bV <= aV && bV <= cV) {
-                    // b is smallest
-                    pivot = aV <= cV ? (left) : (left+2);
-                }else {
-                    // c is smallest
-                    pivot = aV <= bV ? (left) : (left+1);
-                }
-            }
-
-            int val = main[pivot];
-            { // swap pivot with right
-                main[pivot] = main[right];
-                main[right] = val;
-            }
-            {
-                T x = side[pivot];
-                side[pivot] = side[right];
-                side[right] = x;
-            }
-            int L = left, R = right - 1;
-            while(true) {
-                while(L <= R && main[L] >= val) L++; // Reverse order
-                while(L <= R && main[R] <= val) R--;
-                if(L < R) {
-                    { // swap idx L and R
-                        int z = main[L];
-                        main[L] = main[R];
-                        main[R] = z;
-                    }
-                    {
-                        T z = side[L];
-                        side[L] = side[R];
-                        side[R] = z;
-                    }
-                    L++; R--;
-                }else {
-                    break;
-                }
-            }
-            { // swap idx L and right
-                int z = main[L];
-                main[L] = main[right];
-                main[right] = z;
-            }
-            {
-                T z = side[L];
-                side[L] = side[right];
-                side[right] = z;
-            }
-            quickSortReverseOrder(main, side, left, L-1);
-            quickSortReverseOrder(main, side, L+1, right);
+    private <T> void quickSortReverseOrder(int[] main, T[] side, int left, int right) {
+        if(left >= right) return;
+        if(right - left <= 11) {
+            // Do insertion sort
+            insertionSortReverseOrder(main, side, left, right);
+            return;
         }
+        // Choose pivot of median between left, left+1, left+2
+        int pivot;
+        {
+            int aV = main[left], bV = main[left+1], cV = main[left+2];
+            if(aV <= bV && aV <= cV) {
+                // a is smallest
+                pivot = bV <= cV ? (left+1) : (left+2);
+            }else if(bV <= aV && bV <= cV) {
+                // b is smallest
+                pivot = aV <= cV ? (left) : (left+2);
+            }else {
+                // c is smallest
+                pivot = aV <= bV ? (left) : (left+1);
+            }
+        }
+
+        int val = main[pivot];
+        { // swap pivot with right
+            main[pivot] = main[right];
+            main[right] = val;
+        }
+        {
+            T x = side[pivot];
+            side[pivot] = side[right];
+            side[right] = x;
+        }
+        int L = left, R = right - 1;
+        while(true) {
+            while(L <= R && main[L] >= val) L++; // Reverse order
+            while(L <= R && main[R] <= val) R--;
+            if(L < R) {
+                { // swap idx L and R
+                    int z = main[L];
+                    main[L] = main[R];
+                    main[R] = z;
+                }
+                {
+                    T z = side[L];
+                    side[L] = side[R];
+                    side[R] = z;
+                }
+                L++; R--;
+            }else {
+                break;
+            }
+        }
+    { // swap idx L and right
+            int z = main[L];
+            main[L] = main[right];
+            main[right] = z;
+        }
+        {
+            T z = side[L];
+            side[L] = side[right];
+            side[right] = z;
+        }
+        quickSortReverseOrder(main, side, left, L-1);
+        quickSortReverseOrder(main, side, L+1, right);
+    }
+    
     public String[] sortPeople(String[] names, int[] heights) {
         quickSortReverseOrder(heights, names, 0, names.length-1);
         return names;
