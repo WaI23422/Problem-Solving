97a7b1ce5f6fe0360324c61c4f7f09aacf21b8c7
WaI2k2 <buithanh2342002@gmail.com>
2024-07-31 17:25:03 +0700

    Add

---
+++ Java/WebsiteCode/LeechCode/src/BetterCodeAnswer/Medium/Array/FillingBookcaseShelves.java
@@ -0,0 +1,86 @@
+package BetterCodeAnswer.Medium.Array;
+
+/**
+ * <a class="no-underline hover:text-blue-s dark:hover:text-dark-blue-s truncate cursor-text whitespace-normal hover:!text-[inherit]" href="/problems/filling-bookcase-shelves/">1105. Filling Bookcase Shelves</a>
+ * 
+ * <div class="elfjS" data-track-load="description_content"><p>You are given an array <code>books</code> where <code>books[i] = [thickness<sub>i</sub>, height<sub>i</sub>]</code> indicates the thickness and height of the <code>i<sup>th</sup></code> book. You are also given an integer <code>shelfWidth</code>.</p>
+ *
+ * <p>We want to place these books in order onto bookcase shelves that have a total width <code>shelfWidth</code>.</p>
+ * 
+ * <p>We choose some of the books to place on this shelf such that the sum of their thickness is less than or equal to <code>shelfWidth</code>, then build another level of the shelf of the bookcase so that the total height of the bookcase has increased by the maximum height of the books we just put down. We repeat this process until there are no more books to place.</p>
+ * 
+ * <p>Note that at each step of the above process, the order of the books we place is the same order as the given sequence of books.</p>
+ * 
+ * <ul>
+ * 	<li>For example, if we have an ordered list of <code>5</code> books, we might place the first and second book onto the first shelf, the third book on the second shelf, and the fourth and fifth book on the last shelf.</li>
+ * </ul>
+ * 
+ * <p>Return <em>the minimum possible height that the total bookshelf can be after placing shelves in this manner</em>.</p>
+ * 
+ * <p>&nbsp;</p>
+ * <p><strong class="example">Example 1:</strong></p>
+ * <img alt="" src="https://assets.leetcode.com/uploads/2019/06/24/shelves.png" style="height: 500px; width: 337px;">
+ * <pre><strong>Input:</strong> books = [[1,1],[2,3],[2,3],[1,1],[1,1],[1,1],[1,2]], shelfWidth = 4
+ * <strong>Output:</strong> 6
+ * <strong>Explanation:</strong>
+ * The sum of the heights of the 3 shelves is 1 + 3 + 2 = 6.
+ * Notice that book number 2 does not have to be on the first shelf.
+ * </pre>
+ * 
+ * <p><strong class="example">Example 2:</strong></p>
+ * 
+ * <pre><strong>Input:</strong> books = [[1,3],[2,4],[3,2]], shelfWidth = 6
+ * <strong>Output:</strong> 4
+ * </pre>
+ * 
+ * <p>&nbsp;</p>
+ * <p><strong>Constraints:</strong></p>
+ * 
+ * <ul>
+ * 	<li><code>1 &lt;= books.length &lt;= 1000</code></li>
+ * 	<li><code>1 &lt;= thickness<sub>i</sub> &lt;= shelfWidth &lt;= 1000</code></li>
+ * 	<li><code>1 &lt;= height<sub>i</sub> &lt;= 1000</code></li>
+ * </ul>
+ * </div>
+ * 
+ */
+public class FillingBookcaseShelves {
+    public static void main(String[] args) {
+        int[][][][] tests = {
+            {
+                {{4}},
+                {
+                    {1,1},{2,3},{2,3},{1,1},{1,1},{1,1},{1,2},
+                }
+            },
+        };
+
+        for (int[][][] test : tests) {
+            int shelfWidth = test[0][0][0],
+                books[][] = test[1];
+
+            System.out.println(new FillingBookcaseShelves_Solution().minHeightShelves(books, shelfWidth));
+        }
+    }
+}
+
+/**
+ * 0ms 42.56MB
+ */
+class FillingBookcaseShelves_Solution {
+    public int minHeightShelves(int[][] books, int shelfWidth) {
+        int[] dp = new int[books.length + 1];
+        dp[books.length] = 0;
+        for (int i = books.length - 1; i >= 0; i--) {
+            int height = books[i][1];
+            int width = books[i][0];
+            dp[i] = height + dp[i + 1];
+            for (int j = i + 1; j < books.length && (width+books[j][0] <= shelfWidth); j++) {
+                height = Math.max(height, books[j][1]);
+                width += books[j][0];
+                dp[i] = Math.min(dp[i], height + dp[j+1]);
+            }
+        }
+        return dp[0];
+    }
+}
