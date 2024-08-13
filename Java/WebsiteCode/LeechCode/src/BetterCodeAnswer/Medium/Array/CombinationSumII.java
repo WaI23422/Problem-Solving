c151f9d831cde005fee2155214bf672672b1ab0d
WaI2k2 <buithanh2342002@gmail.com>
2024-08-13 11:12:40 +0700

    Fix

--- Java/WebsiteCode/LeechCode/src/BetterCodeAnswer/Medium/Array/CombinationSumII.java
+++ Java/WebsiteCode/LeechCode/src/BetterCodeAnswer/Medium/Array/CombinationSumII.java
@@ -176,3 +176,44 @@ class CombinationSumII_Solution2 {
         }
     }
 }
+
+// 1ms 43.22MB
+class CombinationSumII_Solution3 {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    List<List<Integer>> ans=new ArrayList<>();
+        int[] f =new int[51];
+        int size=0;
+        for(int g:candidates){
+            f[g]++;
+            if(f[g]==1)size++;
+        }
+        int[] h=new int[size];
+        size=0;
+        for(int g=1;g<=50;g++){
+            if(f[g]>0)h[size++]=g;
+        }
+        get(ans,new ArrayList<>(),0,size,0,target,h,f,0);
+        return ans;
+    }
+    static void get(List<List<Integer>> ans, List<Integer> a, int k ,int n, int sum, int t, int[] arr,int[] f, int s){
+        if(k==n || arr[k]==0 || sum+arr[k]>t){
+            return;
+        }
+        if(sum+arr[k]<t){
+            if(f[arr[k]]>0){
+            a.add(arr[k]);
+            f[arr[k]]--;
+            get(ans,a,k,n,sum+arr[k],t,arr,f,s+1);
+            a.remove(s);
+            f[arr[k]]++;
+            }
+            get(ans,a,k+1,n,sum,t,arr,f,s);
+        }else if(f[arr[k]]>0){
+            a.add(arr[k]);
+            ans.add(new ArrayList<>(a));
+            a.remove(s);
+            return;
+        }
+    }
+
+}
